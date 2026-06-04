ad85dca10e030bae73a342ba564ebc9601008543
Matthew Campbell <mcampbell@mageaustralia.com.au>
2026-06-04 15:37:32 +1000

    feat: branding config (system.xml + ACL) and helper getters

--- app/code/community/Mageaustralia/Pdf/Helper/Data.php
+++ app/code/community/Mageaustralia/Pdf/Helper/Data.php
@@ -15,4 +15,37 @@ class Mageaustralia_Pdf_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return (bool) Mage::getStoreConfig('mageaustralia_pdf/general/enabled', $storeId);
     }
+
+    public function getStoreName(?int $storeId = null): string
+    {
+        return (string) Mage::getStoreConfig('mageaustralia_pdf/general/store_name', $storeId);
+    }
+
+    public function getStoreAddress(?int $storeId = null): string
+    {
+        return (string) Mage::getStoreConfig('mageaustralia_pdf/general/store_address', $storeId);
+    }
+
+    public function getAbn(?int $storeId = null): string
+    {
+        return (string) Mage::getStoreConfig('mageaustralia_pdf/general/abn', $storeId);
+    }
+
+    public function getBankDetails(?int $storeId = null): string
+    {
+        return (string) Mage::getStoreConfig('mageaustralia_pdf/general/bank_details', $storeId);
+    }
+
+    /**
+     * Absolute filesystem path to the configured logo, or '' if none/unreadable.
+     */
+    public function getLogoPath(?int $storeId = null): string
+    {
+        $file = (string) Mage::getStoreConfig('mageaustralia_pdf/general/logo', $storeId);
+        if ($file === '') {
+            return '';
+        }
+        $path = Mage::getBaseDir('media') . DS . 'mageaustralia' . DS . 'pdf' . DS . $file;
+        return is_readable($path) ? $path : '';
+    }
 }
